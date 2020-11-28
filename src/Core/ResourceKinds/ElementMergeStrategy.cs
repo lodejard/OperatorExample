23ff8160d3fe32be3efbd875a376af37171110e5
Louis DeJardin <lodejard@microsoft.com>
2020-11-27 20:23:47 -0800

    Adding strategy enum comments (#13)
    Was missed in the last PR

--- src/Core/ResourceKinds/ElementMergeStrategy.cs
+++ src/Core/ResourceKinds/ElementMergeStrategy.cs
@@ -24,9 +24,25 @@ namespace Microsoft.Kubernetes.Core.Resources
         /// Updating dictionary by matching keys.
         /// </summary>
         MergeMap,
+
+        /// <summary>
+        /// Merging list or primatives adding or removing when changed but leaving unmanaged values alone.
+        /// </summary>
         MergeListOfPrimative,
+
+        /// <summary>
+        /// Updating list of primatives by replacing entirely when differences exist.
+        /// </summary>
         ReplaceListOfPrimative,
+
+        /// <summary>
+        /// Merging list of objects when the "merge key" property for object matching is known.
+        /// </summary>
         MergeListOfObject,
+
+        /// <summary>
+        /// Updating list of object by replacing entirely when differences exist.
+        /// </summary>
         ReplaceListOfObject,
     }
 }
